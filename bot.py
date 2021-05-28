e8e4327386d031d168b11a2d22b5fb2743087720
Drucetination <st050025@student.spbu.ru>
2021-05-28 18:26:07 +0300

    fix

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['start', 'help'])
 def welcome(message):
     markup = types.ReplyKeyboardRemove(selective=False)
-    msg = bot.send_message(message.chat.id,
+    bot.send_message(message.chat.id,
                            "Привет, {}, давай изучать английский! С чего начнем?".format(message.from_user.first_name),
                            reply_markup=markup)
     # bot.register_next_step_handler(msg, process_num1_step)
