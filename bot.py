f13a644515252ca66f70ab6f54ee854f0be92422
Drucetination <st050025@student.spbu.ru>
2021-05-29 18:33:38 +0300

    update

--- bot.py
+++ bot.py
@@ -7,26 +7,16 @@ BOT_URL = "https://learn-english-with-me-bot.herokuapp.com/"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def welcome(message):
-    markup = types.ReplyKeyboardRemove(selective=False)
+    markup = types.InlineKeyboardMarkup()
+    words = types.InlineKeyboardButton('Учить слова')
+    statistics = types.InlineKeyboardButton('Вывести статистику')
+    markup.add(words, statistics)
     msg = bot.send_message(message.chat.id,
                            "Привет, {}! Давай изучать английский! С чего начнем?".format(message.from_user.first_name),
                            reply_markup=markup)
-    bot.register_next_step_handler(msg, choose_activity)
-
-
-def choose_activity(message):
-    try:
-        markup = types.ReplyKeyboardMarkup()
-        words = types.KeyboardButton('Учить слова')
-        statistics = types.KeyboardButton('Вывести статистику')
-        markup.add(words, statistics)
-
-        msg = bot.send_message(message.chat.id, "Выбери активность", reply_markup=markup)
-        bot.register_next_step_handler(msg, process_activity_choice)
-    except Exception as e:
-        bot.reply_to(message, "Выбери одну из предложенных активностей")
+    bot.register_next_step_handler(msg, process_activity_choice)
 
 
 def process_activity_choice(message):
