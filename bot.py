a73b9b61503aa230469dcd9184bf5ea04a52f196
Drucetination <st050025@student.spbu.ru>
2021-05-29 18:26:17 +0300

    update

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 import telebot
-import requests
-import os
 from telebot import types
 
 BOT_TOKEN = "1719204229:AAFTEDBCSZSJxYmuApcVkSgjCK4NZY5wNlc"
@@ -12,10 +10,27 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['start', 'help'])
 def welcome(message):
     markup = types.ReplyKeyboardRemove(selective=False)
-    bot.send_message(message.chat.id,
-                     "Привет, {}! Давай изучать английский! С чего начнем?".format(message.from_user.first_name),
-                     reply_markup=markup)
-    # bot.register_next_step_handler(msg, process_num1_step)
+    msg = bot.send_message(message.chat.id,
+                           "Привет, {}! Давай изучать английский! С чего начнем?".format(message.from_user.first_name),
+                           reply_markup=markup)
+    bot.register_next_step_handler(msg, choose_activity)
+
+
+def choose_activity(message):
+    try:
+        markup = types.ReplyKeyboardMarkup()
+        words = types.KeyboardButton('Учить слова')
+        statistics = types.KeyboardButton('Вывести статистику')
+        markup.add(words, statistics)
+
+        msg = bot.send_message(message.chat.id, "Выбери активность", reply_markup=markup)
+        bot.register_next_step_handler(msg, process_activity_choice)
+    except Exception as e:
+        bot.reply_to(message, "Выбери одну из предложенных активностей")
+
+
+def process_activity_choice(message):
+    bot.reply_to(message, "Здесь мы выбираем активность")
 
 
 if __name__ == '__main__':
